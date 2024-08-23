b0f6bad46f4ba04491732c5e0baba848f34627df
Hans Llanos <hans.llanos@gmail.com>
2024-08-23 12:55:14 -0500

    agregando items al carrito

--- practica/src/components/NavBar.jsx
+++ practica/src/components/NavBar.jsx
@@ -1,13 +1,14 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useContext } from "react";
 import { Menubar } from 'primereact/menubar';
 import { Avatar } from 'primereact/avatar';
 import { Badge } from 'primereact/badge';
 import { OverlayPanel } from 'primereact/overlaypanel';
 import { Image } from 'primereact/image';
 import { Button } from 'primereact/button';
+import {  CarritoContext } from "../context/CarritoProvider"
 
 export function NavBar() {    
-
+    const { cantidad, sumarCantidad, restarCantidad, addCarrito, setAddCarrito } = useContext(CarritoContext);
     const op = useRef(null);
 
     const items = [
@@ -33,31 +34,42 @@ export function NavBar() {
         <>
             <div className="icon-carrito">
                 <i className="pi pi-shopping-cart p-overlay-badge" style={{'fontSize': '1.4rem', cursor:'pointer'}}  onClick={(e) => op.current.toggle(e)}>
-                    <Badge value="0"></Badge>
+                    { addCarrito &&
+                        <Badge value={cantidad}></Badge>
+                    }
                 </i>
                 <Avatar image="image-avatar.png" shape="circle"  size="large" />
                 
             </div>
             <OverlayPanel ref={op}>
-            <div className='carrito-detalle'>
-                <div className="titulo">Cart</div>
-                <div className="producto">
-                    <div className="seccion-imagen">
-                        <Image src="./images/galeria/image-product-1-thumbnail.jpg" alt="Image" width="100%" style={{ borderRadius:'10px'}}/>
+                {
+                   addCarrito && 
+                   <div className='carrito-detalle'>
+                        <div className="titulo">Cart</div>
+                        <div className="producto">
+                            <div className="seccion-imagen">
+                                <Image src="./images/galeria/image-product-1-thumbnail.jpg" alt="Image" width="100%" style={{ borderRadius:'10px'}}/>
+                            </div>
+                            <div className="seccion-detalle">
+                                <div className="producto-titulo">Fall Limited Edition Sneakers</div>
+                                <div className="calculo">$125.00 x 3 <span>$375.00</span></div>
+                            </div>
+                            <div className="seccion-eliminar">
+                                <i className="pi pi-trash" style={{'fontSize': '1rem', cursor:'pointer'}} onClick={()=> {setAddCarrito(false)}}>
+                            </i>  
+                            </div>
+                        </div>
+                        <div className="checkout">
+                            <Button label="Checkout" style={{ background:'#d46202', border:'none', marginLeft:'1rem', width:'90%', color:'black' }} />
+                        </div>
                     </div>
-                    <div className="seccion-detalle">
-                        <div className="producto-titulo">Fall Limited Edition Sneakers</div>
-                        <div className="calculo">$125.00 x 3 <span>$375.00</span></div>
+                }
+                { !addCarrito && 
+                    <div className='carrito-detalle'>
+                        <h4 className="vacio">Your Cart is Empty</h4>
                     </div>
-                    <div className="seccion-eliminar">
-                    <i className="pi pi-trash" style={{'fontSize': '1rem', cursor:'pointer'}}>
-                    </i>  
-                    </div>
-                </div>
-                <div className="checkout">
-                    <Button label="Checkout" style={{ background:'#d46202', border:'none', marginLeft:'1rem', width:'90%', color:'black' }} />
-                </div>
-            </div>        
+                }
+                    
         </OverlayPanel>
     </>
     );
